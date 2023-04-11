ab5c4cb4c9f34f10b43cbd57acd781d10d75a331
davidyates31 <davidyates31@gmail.com>
2023-04-11 14:46:50 +0100

    RSG rename

--- network/network.tf
+++ network/network.tf
@@ -1,7 +1,7 @@
 module "network" {
   source              = "Azure/network/azurerm"
   version             = "3.1.1"
-  resource_group_name = "${var.organization_name}RG"
+  resource_group_name = "${var.organization_name}RSG"
   vnet_name           = "${var.org_cidr_block}VNet"
   address_space       = local.az_cidr_block
   subnet_prefixes     = [cidrsubnet(local.az_cidr_block, 8, 0)]

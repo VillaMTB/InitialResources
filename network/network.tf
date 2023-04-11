2be228c2ccd20a3e2369e358d7c30124a55eb542
davidyates31 <davidyates31@gmail.com>
2023-04-11 14:51:12 +0100

    subnet name fix

--- network/network.tf
+++ network/network.tf
@@ -2,7 +2,7 @@ module "network" {
   source              = "Azure/network/azurerm"
   version             = "3.1.1"
   resource_group_name = "${var.organization_name}RSG"
-  vnet_name           = "${var.org_cidr_block}VNet"
+  vnet_name           = "${var.organization_name}VNet"
   address_space       = local.az_cidr_block
   subnet_prefixes     = [cidrsubnet(local.az_cidr_block, 8, 0)]
   subnet_names        = ["corp"]

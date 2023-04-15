fa2e69f99949337584f4001acd11e43b70ba6544
davidyates31 <davidyates31@outlook.com>
2023-04-15 20:54:36 +0000

    created terraform.tf

--- network/terraform.tf
+++ network/terraform.tf
@@ -1,31 +1,8 @@
-
-terraform {
-  cloud {
-    organization = "VillaMTB"
-    workspaces {
-      name = "InitialResources"
+  terraform {
+    backend "remote" {
+      organization = "$(TFC_ORG)"
+      workspaces {
+        name = ""
+      }
     }
   }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.0"
-    }
-    oci = {
-      source = "hashicorp/oci"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-provider "oci" {
-  region       = var.oci_region
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.oci_fingerprint
-  private_key  = var.oci_private_key
-}
-
-data "azurerm_client_config" "current" {}

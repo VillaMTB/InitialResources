6a7d6e3e8123882f5dd26eb14f31d7611aefa6ea
davidyates31 <davidyates31@gmail.com>
2023-04-16 11:21:23 +0100

    Moved DevOps Setup

--- network/terraform.tf
+++ network/terraform.tf
@@ -1,8 +1,27 @@
-  terraform {
-    backend "remote" {
-      organization = "$(TFC_ORG)"
-      workspaces {
-        name = ""
-      }
+terraform {
+  cloud {
+    organization = "$(TFC_ORG)"
+    workspaces {
+      name = "InitialResources"
     }
   }
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+
+    }
+  }
+}
+
+provider "azuredevops" {
+  org_service_url = local.ado_org_service_url
+  # Authentication through PAT defined with AZDO_PERSONAL_ACCESS_TOKEN 
+}
+provider "azurerm" {
+  features {}
+}

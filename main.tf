062ef43f41ecf10e113d9f6e8413f1e67388edc2
davidyates31 <davidyates31@gmail.com>
2023-06-02 12:33:49 +0100

    on prem cidr block

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "oci-network" {
   az_cidr_block        = cidrsubnet(cidrsubnet(var.org_cidr_block, 8, 1), 8, 0)
   environment_code     = "Corp"
   home_ip_address      = "2.25.217.51"
-  onprem_cidr_block    = cidrsubnet(cidrsubnet(var.org_cidr_block, 8, 5), 8, 0)
+  onprem_cidr_block    = cidrsubnet(var.org_cidr_block, 8, 5)
   org_cidr_block       = var.org_cidr_block
   org_compartment_ocid = local.org_compartment_ocid
   organization_name    = var.organization_name

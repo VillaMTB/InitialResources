6975628f90d939bd9181ebbfef0105e2c8871361
David Yates <david@villamtb.com>
2023-08-22 07:21:46 +0100

    IP address change

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "oci-network" {
   source               = "./oci/network"
   az_cidr_block        = cidrsubnet(cidrsubnet(var.org_cidr_block, 8, 1), 8, 0)
   environment_code     = "Corp"
-  home_ip_address      = "95.148.17.115"
+  home_ip_address      = "95.144.166.241"
   onprem_cidr_block    = cidrsubnet(var.org_cidr_block, 8, 5)
   org_cidr_block       = var.org_cidr_block
   org_compartment_ocid = local.org_compartment_ocid
